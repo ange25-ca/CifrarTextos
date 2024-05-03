95639fe7a86bf3ad28c21e704069e71860d41919
AngelicaCahum2525 <monsecahuum@gmail.com>
2024-05-03 15:25:52 -0600

    1/2 de navbar (Prueba)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 //Exihibe la pagína al puerto destinado
 router.get('/', (req, res, next) => {
-    res.render('index', {title: 'BIENVENIDOS'});
+    res.render('index', {title: ' CODETEXT'});
     next() //Permite continuar con el middleware de express
   });
 
